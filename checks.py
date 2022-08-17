2c5e1a020806fd1ce74984a6efc2d8d972acb7a3
Esther Csoke <esther.e.csoke@gmail.com>
2022-08-17 16:14:15 +1000

    added files for project

---
+++ checks.py
@@ -0,0 +1,21 @@
+import datetime
+import sys
+
+def check_arguments(args):
+ if len(args) != 3:
+    print("[ERROR] You need to provide 3 arguments in the following order: <date> <currency1> <currency2>")
+    sys.exit(0)
+    
+    # check if 3 arguments
+    # check if arguments are in correct order
+    
+
+
+def check_date(date):
+    try:
+        date_check = datetime.datetime.strptime(date, '%Y-%m-%d')  
+        print(date_check)
+    except:
+        print("Date Format is Wrong")
+
+  
